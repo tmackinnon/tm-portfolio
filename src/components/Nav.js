277fb97605d423121bdcd72121c7f18d6ba9ce7c
Tara MacKinnon <taramackinnon4@gmail.com>
2023-06-16 13:43:59 -0700

    start page layout, add structure of nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+export default function Nav() {
+
+
+  return (
+    <div className="flex justify-between m-0 bg-gray-200 w-screen p-4 items-center">
+      <h1 className="text-5xl font-bold">Tara MacKinnon</h1>
+      <div className="flex">
+        <h3 className="p-4 text-2xl font-semibold">About</h3>
+        <h3 className="p-4 text-2xl font-semibold">Projects</h3>
+        <h3 className="p-4 text-2xl font-semibold">Contact</h3>
+      </div>
+    </div>
+  );
+}
