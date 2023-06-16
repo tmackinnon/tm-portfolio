10f40573c0199bc14e92eb5f29fdecc75fd96644
Tara MacKinnon <taramackinnon4@gmail.com>
2023-06-16 14:24:10 -0700

    add about blurb

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,14 @@ export default function About() {
   return (
     <div>
       <h1>About Me</h1>
+      <div>
+        <h2>Hi there! I&apos;m Tara, a full-stack web developer based in Vancouver, BC.</h2>
+        <p>
+          With a background in fundraising and not-for-profits, my desire to make a meaningful difference has been a constant driving force.
+          As I transition into a career in tech, that motivation persists and I am inspired by the industry&apos;s potential to address the social and environmental challenges of today.
+          I firmly believe that the power of tech can be harnessed to create innovative solutions and I am excited to play a role in driving that positive change.
+        </p>
+      </div>
     </div>
   );
 }
