392478ea859e28414235ba4beb2d645af805ac09
Tara MacKinnon <taramackinnon4@gmail.com>
2023-06-20 12:59:12 -0700

    add fonts

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import Button from "./Button";
+// import { Shrikhand } from 'next/font/google';
+// const shrikhand = Shrikhand({ subsets: ['latin'], weight: '400' });
+
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
-      <div>
+      <h1 className='text-5xl font-bold font-sans'>About Me</h1>
+      <div className='text-xl'>
         <h2>Hi there! I&apos;m Tara, a full-stack web developer based in Vancouver, BC.</h2>
         <p>
           With a background in fundraising and not-for-profits, my desire to make a meaningful difference has been a constant driving force.
@@ -10,6 +14,7 @@ export default function About() {
           I firmly believe that the power of tech can be harnessed to create innovative solutions and I am excited to play a role in driving that positive change.
         </p>
       </div>
+      <Button title='Learn More'/>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ export default function Nav() {
 
   return (
     <div className="flex justify-between m-0 bg-gray-200 w-screen p-4 items-center">
-      <h1 className="text-5xl font-bold">Tara MacKinnon</h1>
+      <h1 className={`text-5xl font-sans`}>Tara MacKinnon</h1>
       <div className="flex">
         <h3 className="p-4 text-2xl font-semibold">About</h3>
         <h3 className="p-4 text-2xl font-semibold">Projects</h3>
