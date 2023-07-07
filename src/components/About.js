6b49583b17bbe163366bdd2ccd56b636fa7a1178
Tara MacKinnon <taramackinnon4@gmail.com>
2023-07-07 13:23:37 -0700

    fix styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,26 +6,33 @@ import profile from '../../public/profile.png';
 
 export default function About() {
   return (
-    <div id='About' className="flex py-20 mt-48 w-3/4">
-      <div className="mr-20">
-        <h1 className='text-6xl font-bold font-sans mb-4'>About Me</h1>
-        <h2 className='text-4xl mb-4'>
-          Hi there! I&apos;m Tara, a full-stack web developer based in Vancouver, BC.
-        </h2>
-        <p className='text-2xl mb-4'>
-            With a background in fundraising and not-for-profits, my desire to make a meaningful difference has been a constant driving force.
-            As I transition into a career in tech, that motivation persists and I am inspired by the industry&apos;s potential to address the social and environmental challenges of today.
-            I firmly believe that the power of tech can be harnessed to create innovative solutions and I am excited to play a role in driving that positive change.
-        </p>
-        <Button title='Learn More'/>
+    <div id='About' className="flex py-80 bg-gray-200 justify-center">
+
+      <div className="flex items-center justify-evenly w-3/4">
+
+        <div className="w-3/5">
+          <h1 className='text-6xl font-bold font-sans mb-4'>About Me</h1>
+          <h2 className='text-4xl mb-4'>
+            Hi there! I&apos;m Tara, a full-stack web developer based in Vancouver, BC.
+          </h2>
+          <p className='text-2xl mb-4'>
+              With a background in fundraising and not-for-profits, my desire to make a meaningful difference has been a constant driving force.
+              As I transition into a career in tech, that motivation persists and I am inspired by the industry&apos;s potential to address the social and environmental challenges of today.
+              I firmly believe that the power of tech can be harnessed to create innovative solutions and I am excited to play a role in driving that positive change.
+          </p>
+          <Button title='Learn More'/>
+        </div>
+        <div>
+          <Image
+            className='rounded-full'
+            src={profile}
+            width={400}
+            height={400}
+            alt="Picture of the author"
+          />
+
+        </div>
       </div>
-      <Image
-        className='rounded-full w-2/5 h-2/5'
-        src={profile}
-        width={350}
-        height={350}
-        alt="Picture of the author"
-      />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -26,13 +26,13 @@ export default function Nav() {
  
 
   return (
-    <div className={`flex fixed top-0 left-0 right-0 m-0 w-screen h-28 items-center justify-center ${scroll ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}>
+    <div className={`flex fixed top-0 left-0 right-0 m-0 w-screen h-28 items-center justify-center shadow-lg ${scroll ? 'bg-primary text-secondary border-secondary' : 'bg-secondary text-primary border-primary'}`}>
       <div className="flex justify-between m-0 w-3/4 min-w-min items-center">
         <h1 className='text-6xl font-sans'>Tara MacKinnon</h1>
         <div className="flex">
-          <Link href="#About" className="p-4 text-2xl font-semibold">About</Link>
-          <Link href="#Projects" className="p-4 text-2xl font-semibold">Projects</Link>
-          <h3 className="p-4 text-2xl font-semibold">Contact</h3>
+          <Link href="#About" className="m-4 text-2xl font-semibold hover:border-b-2">About</Link>
+          <Link href="#Projects" className="m-4 text-2xl font-semibold hover:border-b-2">Projects</Link>
+          <h3 className="m-4 text-2xl font-semibold hover:border-b-2">Contact</h3>
         </div>
       </div>
     </div>
