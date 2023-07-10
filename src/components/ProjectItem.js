5e21218e0b22c91bd71aeb50121f04260b190f64
Tara MacKinnon <taramackinnon4@gmail.com>
2023-07-10 12:12:25 -0700

    replace screenshot

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,18 +4,20 @@ import Link from 'next/link';
 export default function ProjectItem(props) {
 
   return (
-    <div className='flex flex-col items-center w-2/5 shadow-lg m-6 h-700px'>
-      <Link  href={props.link} className='flex flex-col items-center p-6 w-full bg-blue-50 rounded justify-center' style={{width: '100%', height: '500px'}}>
-        <Image
-          src={props.image}
-          alt="Petal Profits Homepage Screenshot"
-          width={600}
-          height={400}
-        />
-      </Link>
-      <div className='flex flex-col w-full mt-1 p-4 items-start'>
-        <h3 className='text-xl font-bold'>{props.title}</h3>
-        <p className='mt-2'>{props.description}<br></br><b>Tech Stack:</b> NodeJS, Express, ReactJS, PostgreSQL, SASS</p>
+    <div className='flex flex-col items-center w-2/5 shadow-lg rounded-lg my-20 h-700px bg-primary pt-2'>
+      <div className='p-1 bg-gra w-4/5'>
+        <Link  href={props.link} className='flex flex-col items-center w-full justify-center rounded-none' style={{width: '100%'}}>
+          <Image
+            src={props.image}
+            alt="Homepage Screenshot"
+            width={600}
+            height={400}
+          />
+        </Link>
+      </div>
+      <div className='flex flex-col w-full mb-2 px-4 pt-4 pb-8 items-startd text-gray-700 w-4/5'>
+        <h3 className='text-3xl text-center font-bold border-b-2'>{props.title}</h3>
+        <p className='mt-6 text-xl text-center'>{props.description}<br></br><b>Tech Stack:</b> NodeJS, Express, ReactJS, PostgreSQL, SASS</p>
       </div>
     </div>
   );
